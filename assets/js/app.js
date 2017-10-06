e0902330660675eea57cadf50509d84c304fe486
bariflor <barihuamachuco@gmail.com>
2017-10-06 07:06:21 -0500

    modifique pagina

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,9 @@
-// $(document).ready(function(){
-// 	$("#equis1"),click(function(){
-// 		$(this).hide();
-// 	});
-// });
-
 $(document).ready(function(){
 	$("#mostrar").on( "click", function() {
-		$('#contenedor').show(); //muestro mediante clase
+		$('.card').show(); //muestro mediante clase
 		});
 	$("#ocultar").on( "click", function() {
-		$('#contenedor').hide(); //muestro mediante clase
+		$('.card').hide(); //muestro mediante clase
 	});
-	// $("#ocultar").on( "click", function() {
-	// 	$('#contenedor').hide(); //muestro mediante clase
-	// });
+
 });
-
-
-// USAR animate.css
-// .add Class("animate Fade Out");
