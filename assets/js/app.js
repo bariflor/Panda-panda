235224ff3dbd828f948e7ba93aa99bfae17003da
bariflor <barihuamachuco@gmail.com>
2017-10-06 10:37:02 -0500

    añadi funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,14 @@ $(document).ready(function(){
 	$("#ocultar").on( "click", function() {
 		$('.card').hide(); //muestro mediante clase
 	});
-
+	$("#cerrar1").click(function(){
+    $("#car1").hide(1000);
+    });
+    $("#cerrar2").click(function(){
+    $("#car2").hide(1000);
+    });
+    $("#cerrar3").click(function(){
+    $("#car3").hide(1000);
+    });
 });
+
